bd28cc476c2fc76978630e756e27c4bcd58f8e9c
Amlal El Mahrouss <amlal@nekernel.org>
2025-12-17 14:51:01 +0100

    feat: Add file structure.
    Signed-off-by: Amlal El Mahrouss <amlal@nekernel.org>

---
+++ include/ocl/tproc.hpp
@@ -0,0 +1,12 @@
+// Copyright 2025, Amlal El Mahrouss (amlal@nekernel.org)
+// Distributed under the Boost Software License, Version 1.0. (See accompanying
+// file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+// Official repository: https://github.com/ocl-org/tproc
+
+#ifndef OCL_TPROC_HPP
+#define OCL_TPROC_HPP
+
+#include <ocl/tproc/detail/config.hpp>
+#include <ocl/tproc/rope.hpp>
+
+#endif // OCL_TPROC_HPP
