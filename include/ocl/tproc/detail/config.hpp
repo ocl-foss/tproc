33cbf6971da84801c489ce5ca19ba18867c0b7e1
Amlal El Mahrouss <amlal@nekernel.org>
2025-12-19 09:00:30 +0100

    chore: implement config.hpp, add MailMap, compile_flags, and format files.
    Signed-off-by: Amlal El Mahrouss <amlal@nekernel.org>

---
+++ include/ocl/tproc/detail/config.hpp
@@ -0,0 +1,20 @@
+/*
+ * File: config.hpp
+ * Purpose: Configuration header of the `tproc` library.
+ * Author: Amlal El Mahrouss (amlal@nekernel.org)
+ * Copyright 2025, Amlal El Mahrouss, licensed under the Boost Software License.
+ */
+
+#ifndef __OCL_TPROC_CONFIG
+#define __OCL_TPROC_CONFIG
+
+#include <ocl/detail/config.hpp>
+#include <ocl/is_same.hpp>
+#include <ocl/option.hpp>
+#include <ocl/smart_ptr.hpp>
+
+namespace ocl {
+template <typename CharT> class basic_rope;
+}
+
+#endif // ifndef __OCL_TPROC_CONFIG
