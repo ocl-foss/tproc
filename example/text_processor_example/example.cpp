6bfb620839216b11659ba83a741358fe27231279
Amlal El Mahrouss <amlal@nekernel.org>
2026-03-02 07:15:33 +0100

    wip: TextProcessing Example using Qt.
    Signed-off-by: Amlal El Mahrouss <amlal@nekernel.org>

--- example/text_processor_example/example.cpp
+++ example/text_processor_example/example.cpp
@@ -5,10 +5,6 @@
  * Copyright 2026, Amlal El Mahrouss, licensed under the Boost Software License.
  */
 
-#include <ocl/tproc/rope.hpp>
-#include <iostream>
-#include <memory>
-
 #include "qt_widget.hpp"
 
 int main(int argc, char** argv)
