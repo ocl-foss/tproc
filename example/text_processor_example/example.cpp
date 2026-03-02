7321d56a6f3667899ac6d624d08750e656517bde
Amlal El Mahrouss <amlal@nekernel.org>
2026-03-02 07:15:12 +0100

    wip: TextProcessing Example using Qt.
    Signed-off-by: Amlal El Mahrouss <amlal@nekernel.org>

---
+++ example/text_processor_example/example.cpp
@@ -0,0 +1,19 @@
+/*
+ * File: example.cpp
+ * Purpose: Rope example.
+ * Author: Amlal El Mahrouss (amlal@nekernel.org)
+ * Copyright 2026, Amlal El Mahrouss, licensed under the Boost Software License.
+ */
+
+#include <ocl/tproc/rope.hpp>
+#include <iostream>
+#include <memory>
+
+#include "qt_widget.hpp"
+
+int main(int argc, char** argv)
+{
+  QApplication app(argc, argv);
+
+  return app.exec();
+}
---
+++ example/text_processor_example/qt_widget.hpp
@@ -0,0 +1,50 @@
+/*
+ * File: qt_widget.hpp
+ * Purpose: Minimal text editor widget example using tproc rope.
+ * Author: Amlal El Mahrouss (amlal@nekernel.org)
+ * Copyright 2026, Amlal El Mahrouss, licensed under the Boost Software License.
+ */
+
+#ifndef TPROC_EXAMPLE_QT_WIDGET_HPP
+#define TPROC_EXAMPLE_QT_WIDGET_HPP
+
+#include <ocl/tproc/rope.hpp>
+
+#include <iostream>
+#include <string>
+#include <string_view>
+#include <QWidget>
+#include <QApplication>
+
+#ifndef STANDALONE
+
+using namespace ocl;
+using namespace boost;
+
+#else
+
+using namespace boost;
+
+#endif
+
+class TTextEditorWidget : public QWidget
+{
+    Q_OBJECT
+
+public:
+	using rope_type  = tproc::crope;
+	using size_type  = rope_type::size_type;
+	using string_view = std::string_view;
+
+	TTextEditorWidget(string_view text = {})
+		: buffer_(text)
+	{
+	}
+
+    virtual ~TTextEditorWidget() = default;
+
+private:
+	rope_type buffer_;
+};
+
+#endif
