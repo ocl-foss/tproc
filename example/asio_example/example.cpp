d363f0dc2325a1811b009ec0f96952f66ce100e9
Amlal El Mahrouss <amlal@nekernel.org>
2026-03-09 05:55:14 +0100

    [CHORE] Add new TProc example.
    Signed-off-by: Amlal El Mahrouss <amlal@nekernel.org>

---
+++ example/asio_example/example.cpp
@@ -0,0 +1,39 @@
+/*
+ * File: example.cpp
+ * Purpose: Rope example.
+ * Author: Amlal El Mahrouss (amlal@nekernel.org)
+ * Copyright 2025-2026, Amlal El Mahrouss, licensed under the Boost Software License.
+ */
+
+#include <ocl/tproc/rope.hpp>
+#include <ocl/asio.hpp>
+#include <iostream>
+#include <memory>
+#include <future>
+
+#ifndef STANDALONE
+using namespace ocl;
+#else
+using namespace boost;
+#endif
+
+int main()
+{
+	auto rope	  = tproc::crope("The Quick Brown Fox Jumps Over The Lazy Dog");
+	auto new_elem = std::make_unique<tproc::crope>(", and Jumps again");
+	auto new_elem_2 = std::make_unique<tproc::crope>(", and then Jumps down.");
+	
+	boost::asio::io_context ioc{1};
+	auto					spawn_strand = boost::asio::make_strand(ioc);
+	
+	boost::asio::co_spawn(spawn_strand, [&new_elem, &new_elem_2, &rope]() -> boost::asio::awaitable<void> {
+	    rope.concat(new_elem.get());    
+	    new_elem->concat(new_elem_2.get());
+	    
+	    co_return;
+	}, boost::asio::detached);
+
+	ocl::asio::run<[]() { (void)0; }>(ioc);
+	  
+	std::cout << rope << std::endl;
+}
